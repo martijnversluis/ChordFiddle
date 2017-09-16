e7898e7f688e955e66e8efb2764c4c62bdcacbf1
Martijn Versluis <martijnversluis@users.noreply.github.com>
2017-09-16 13:03:53 +0200

    Add button for transposing up and down

---
+++ js/chord_pro_editor.js
@@ -0,0 +1,57 @@
+import ChordSheetJS from 'chordsheetjs';
+import Chord from 'chordjs';
+
+export default class ChordFiddle {
+  constructor({editor, previewer = null}) {
+    this.editor = editor;
+    this.previewer = previewer;
+
+    if (this.previewer) {
+      this.editor.addEventListener('input', this.onEditorChange.bind(this));
+
+      if (this.editor.value.length) {
+        this.onEditorChange();
+      }
+    }
+  }
+
+  onEditorChange() {
+    const song = this.parseChordProSheet();
+    const formatter = new ChordSheetJS.TextFormatter();
+    const chordSheet = formatter.format(song);
+    this.previewer.value = chordSheet;
+  }
+
+  parseChordProSheet() {
+    const parser = new ChordSheetJS.ChordProParser();
+    return parser.parse(this.editor.value);
+  }
+
+  transitChords(callback) {
+    const formatter = new ChordSheetJS.ChordProFormatter();
+    const song = this.parseChordProSheet();
+
+    song.lines.forEach(line => {
+      line.items.forEach(item => {
+        if (item instanceof ChordSheetJS.ChordLyricsPair && item.chords) {
+          const parsedChord = Chord.parse(item.chords);
+          item.chords = callback(parsedChord).toString();
+        }
+
+        return item;
+      });
+    });
+
+    const transposedSong = formatter.format(song);
+    this.editor.value = transposedSong;
+    this.onEditorChange();
+  }
+
+  transposeUp() {
+    this.transitChords(chord => chord.transposeUp());
+  }
+
+  transposeDown() {
+    this.transitChords(chord => chord.transposeDown());
+  }
+}
