896486f5573e9724f5f7189c8008c8eab493d049
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-04-08 21:25:52 +0200

    Solve Toolbar issues

---
+++ src/Toolbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Toolbar.css';
+
+export default function Toolbar(props) {
+  const { onTransposeDown, onTransposeUp, onSwitchToSharp, onSwitchToFlat, onShowImportChordSheetDialog } = props;
+
+  return (
+    <ul className="Toolbar">
+      <li>
+        <button onClick={onTransposeDown}>Transpose down</button>
+      </li>
+
+      <li>
+        <button onClick={onTransposeUp}>Transpose up</button>
+      </li>
+
+      <li>
+        <button onClick={onSwitchToSharp}>Use ♯</button>
+      </li>
+
+      <li>
+        <button onClick={onSwitchToFlat}>Use ♭</button>
+      </li>
+
+      <li>
+        <button onClick={onShowImportChordSheetDialog}>Import chord sheet</button>
+      </li>
+    </ul>
+  );
+}
