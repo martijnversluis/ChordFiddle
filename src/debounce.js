ce1832cdc37caca7a699ae650c85c25b32ad0d6b
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-04-14 20:50:53 +0200

    Solve Eslint issues for debounce

--- src/debounce.js
+++ src/debounce.js
@@ -1,10 +1,10 @@
 export default function debounce(func, interval = 100) {
-  return function (...args) {
-    if (this._debounceTimer) {
-      clearTimeout(this._debounceTimer);
+  return function debouncedFunction(...args) {
+    if (this.debounceTimer) {
+      clearTimeout(this.debounceTimer);
     }
 
     const boundFunction = func.bind(null, args);
-    this._debounceTimer = setTimeout(boundFunction, interval);
+    this.debounceTimer = setTimeout(boundFunction, interval);
   };
 }
