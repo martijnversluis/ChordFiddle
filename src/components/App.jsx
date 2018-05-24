c33a3c8232ef69c872da31d623de9fcdbfe04b0c
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-05-24 07:48:37 +0200

    Change App to stateless component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
@@ -8,37 +8,20 @@ import EditorColumn from './EditorColumn';
 import '../css/App.css';
 import ViewerColumn from './ViewerColumn';
 
-class App extends Component {
-  componentDidUpdate() {
-    this.updateLocationHash();
-  }
+const App = () => (
+  <div className="App">
+    <Header />
 
-  updateLocationHash = debounce(() => {
-    const { chordSheet, previewMode } = this.props;
-
-    window.location.hash = queryString.stringify({
-      preview: previewMode,
-      chord_sheet: compress(chordSheet),
-    });
-  });
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-
-        <main className="App__container">
-          <div className="App__columns">
-            <EditorColumn />
-            <ViewerColumn />
-          </div>
-        </main>
-
-        <ImportDialogContainer />
+    <main className="App__container">
+      <div className="App__columns">
+        <EditorColumn />
+        <ViewerColumn />
       </div>
-    );
-  }
-}
+    </main>
+
+    <ImportDialogContainer />
+  </div>
+);
 
 App.propTypes = {
   chordSheet: PropTypes.string.isRequired,
