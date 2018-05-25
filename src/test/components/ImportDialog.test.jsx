395ca4ee694515d7fcdf6d4d81b56b7f51abf615
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-05-25 21:20:09 +0200

    Add test for Navigation

--- src/test/components/ImportDialog.test.jsx
+++ src/test/components/ImportDialog.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { configure as configureEnzyme, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 import ImportDialog from '../../components/ImportDialog';
 
 configureEnzyme({ adapter: new Adapter() });
