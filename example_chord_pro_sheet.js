937f66e4d51ea4c5ea779231007d499989843916
Martijn Versluis <martijnversluis@users.noreply.github.com>
2022-12-31 17:11:42 +0100

    Showcase chordpro font directives

---
+++ example_chord_pro_sheet.js
@@ -0,0 +1,26 @@
+module.exports = `
+{title: Let it be}
+{subtitle: ChordSheetJS example version}
+{key: C}
+{capo: 5}
+{composer: John Lennon}
+{composer: Paul McCartney}
+{chordsize: 12px}
+
+{textcolour: maroon}
+Written by: %{composer}
+{textcolour}
+Lyrics by: %{lyricist|%{}|(unknown)}
+
+{chordcolour: purple}
+{chordfont: monospace}
+{chordsize: 150%}
+{textcolour: navy}
+{textfont: sans-serif}
+{textsize: 120%}
+
+{start_of_chorus}
+Let it [Am]be, let it [C/G]be, let it [F]be, let it [C]be
+[C]Whisper words of [G]wisdom, let it [F]be [C/E] [Dm] [C]
+{end_of_chorus}
+`;
