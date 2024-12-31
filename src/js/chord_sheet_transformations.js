4b0b55700f97408c76d3115b8b11a1b88473c39c
Martijn Versluis <martijnversluis@users.noreply.github.com>
2024-12-31 12:46:40 +0100

    Update dependencies and layout

--- src/js/chord_sheet_transformations.js
+++ src/js/chord_sheet_transformations.js
@@ -1,57 +1,34 @@
-import ChordSheetJS from 'chordsheetjs';
-import { parse as parseChord } from 'chordjs';
-
-const processChord = (item, processor) => {
-  if (item instanceof ChordSheetJS.ChordLyricsPair && item.chords) {
-    const parsedChord = parseChord(item.chords);
-
-    if (parsedChord) {
-      const processedChordLyricsPair = item.clone();
-      processedChordLyricsPair.chords = processor(parsedChord).toString();
-      return processedChordLyricsPair;
-    }
-  }
-
-  return item;
-};
-
-const transformSong = (song, processor) => {
-  const processedSong = song.clone();
-
-  processedSong.lines = song.lines.map((line) => {
-    const processedLine = line.clone();
-    processedLine.items = line.items.map((item) => processChord(item, processor));
-    return processedLine;
-  });
-
-  return processedSong;
-};
+import {
+  ChordProFormatter,
+  ChordProParser,
+  ChordSheetParser,
+} from 'chordsheetjs';
 
 const transformChordSheet = (chordSheet, processor) => {
-  const song = new ChordSheetJS.ChordProParser().parse(chordSheet);
-  const processedSong = transformSong(song, processor);
-  return new ChordSheetJS.ChordProFormatter().format(processedSong);
+  const song = new ChordProParser().parse(chordSheet);
+  const processedSong = processor(song);
+  return new ChordProFormatter().format(processedSong);
 };
 
 export const transposeUp = (chordSheet) => (
-  transformChordSheet(chordSheet, (chord) => chord.transposeUp())
+  transformChordSheet(chordSheet, (song) => song.transposeUp())
 );
 
 export const transposeDown = (chordSheet) => (
-  transformChordSheet(chordSheet, (chord) => chord.transposeDown())
+  transformChordSheet(chordSheet, (song) => song.transposeDown())
 );
 
 export const switchToSharp = (chordSheet) => (
-  transformChordSheet(chordSheet, (chord) => chord.useModifier('#'))
+  transformChordSheet(chordSheet, (song) => song.useModifier('#'))
 );
 
 export const switchToFlat = (chordSheet) => (
-  transformChordSheet(chordSheet, (chord) => chord.useModifier('b'))
+  transformChordSheet(chordSheet, (song) => song.useModifier('b'))
 );
 
 export const convertChordSheetToChordPro = (chordSheet) => {
-  const parser = new ChordSheetJS.ChordSheetParser({ preserveWhitespace: false });
-  const formatter = new ChordSheetJS.ChordProFormatter();
+  const parser = new ChordSheetParser({ preserveWhitespace: false });
+  const formatter = new ChordProFormatter();
   const song = parser.parse(chordSheet);
   return formatter.format(song);
 };
